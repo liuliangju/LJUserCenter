5f749c36cd5fafb885f88b92449db4fd4a18bacd
liuliangju <liangjuliu@163.com>
2017-04-01 07:32:52 +0800

    add pod files

--- LJUserCenter/Classes/LJUserCenterTableViewCell.h
+++ LJUserCenter/Classes/LJUserCenterTableViewCell.h
@@ -15,6 +15,13 @@ static const CGFloat kSectionHeaderAndFooterMarginLeft = 15.0f;
 @protocol LJUserCenterTableViewCellDelegate <NSObject>
 @optional
 
+/**
+ *  checked有变化时调用该方法，子类如果关注则需要继承该方法
+ *
+ *  @param checked   选中则为YES否则为NO
+ *  @param cellModel cell数据model
+ *  @param cell      点击的cell
+ */
 - (void)didCheckChanged:(BOOL)checked
           withCellModel:(LJUserCenterCellModel *)cellModel
                  atCell:(LJUserCenterTableViewCell *)cell;
